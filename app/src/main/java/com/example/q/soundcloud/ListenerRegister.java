23a76e13b08653663fa9539cc5e1554fcbbe92b3
JuHee <dlwngml0504@naver.com>
2016-07-11 14:59:16 +0900

    spinner도입부분

--- app/src/main/java/com/example/q/soundcloud/ListenerRegister.java
+++ app/src/main/java/com/example/q/soundcloud/ListenerRegister.java
@@ -2,12 +2,21 @@ package com.example.q.soundcloud;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class ListenerRegister extends AppCompatActivity {
+    private Spinner mSpinner = null;
+    // 스피너에 뿌려질 Array형식의 Data를 담을 Adapter
+    private ArrayAdapter<String> mSpinnerAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_listener);
+
     }
 }
